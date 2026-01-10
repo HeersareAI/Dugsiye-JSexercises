a05b256f1c41c69b06dfd27e2dac88b3199f29f4
HeersareAI <Heersareai@gmail.com>
2026-01-10 16:29:21 +0100

    exercise4

---
+++ exercise4/main.js
@@ -0,0 +1,16 @@
+function add(a,b) {
+    return a+b;
+} 
+let result = add(20,10);
+console.log(result / 2)
+console.log(result + 10)
+console.log(result * 2)
+
+//function expression
+const Hi = function(a,b){
+    console.log(a * b)
+}
+Hi(10,15);
+Hi(2,12);
+Hi(3,21);
+
