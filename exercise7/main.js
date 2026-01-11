66c814d9a18b1f8deb8b50a01b4a8f05bb1371ef
HeersareAI <Heersareai@gmail.com>
2026-01-11 14:11:37 +0100

    exercise7

---
+++ exercise7/main.js
@@ -0,0 +1,17 @@
+let colors = ["Black", "White", "Red", "Yellow",];
+//push
+colors.push("Green");
+console.log(colors);
+//pop
+colors.pop();
+console.log(colors);
+//unshift
+colors.unshift("Blue");
+console.log(colors);
+//shift
+colors.shift();
+console.log(colors);
+colors.shift();
+console.log(colors);
+//length
+console.log(colors.length);
