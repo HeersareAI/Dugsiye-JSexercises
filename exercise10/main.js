b9ee25a7c84faa1067cac565a07f7b77630f8645
HeersareAI <Heersareai@gmail.com>
2026-01-11 16:34:21 +0100

    exercise10

---
+++ exercise10/main.js
@@ -0,0 +1,20 @@
+//Arrys of objects
+let library = [
+    {
+        title: 'scan pop play',
+        author: 'Pringles',
+        year: '2026',
+    },
+    {
+        title: 'pop scan play',
+        author: 'Pringles',
+        year: '2026',
+    },
+    {
+        title: 'pop play scan',
+        author: 'Pringles',
+        year: '2026',
+    },
+]
+console.log(library[0].title);
+console.log(library[1].author);
