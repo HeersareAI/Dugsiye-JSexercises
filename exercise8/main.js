3bee1288669638976e2a48083dfa8c9b17a59126
HeersareAI <Heersareai@gmail.com>
2026-01-11 14:51:49 +0100

    exercise8

---
+++ exercise8/main.js
@@ -0,0 +1,16 @@
+//Objects
+let Car = {
+    Make: 'Volvo',
+    Model: 'V80',
+    Year: 2019,
+}
+console.log(Car);
+//DOT
+console.log(Car.Make);
+console.log(Car.Model);
+console.log(Car.Year);
+
+//OFF
+console.log(Car['Make'])
+console.log(Car['Model'])
+console.log(Car['Year'])
