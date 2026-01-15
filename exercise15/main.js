4b942617a5de9f8e4b5a3742873c82727968f591
HeersareAI <Heersareai@gmail.com>
2026-01-15 18:08:32 +0100

    exercise15

---
+++ exercise15/main.js
@@ -0,0 +1,17 @@
+//For in Loop
+const data = {name: "name : Ahmed", age: "age 34", city: "city : Östersund"};
+for(let key in data){
+    console.log(data[key])
+}
+console.log("-------")
+
+const tariiq = {name: "name : Osman ", age: "age 24", city: "city : Umeå"};
+for(let key in tariiq){
+    console.log(tariiq[key])
+}
+console.log("-------")
+
+const trt = {name: "name : Jamac", age: "age 23", city: "city : Upsala"};
+for(let key in trt){
+    console.log(trt[key])
+}
