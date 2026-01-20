236aa3dbf2ae57954ad19504d27c93e1d9f60bf1
HeersareAI <Heersareai@gmail.com>
2026-01-20 16:36:59 +0100

    exercise17

---
+++ exercise17/main.js
@@ -0,0 +1,11 @@
+let temperature = 22;
+
+if( temperature < -2) {
+    console.log("Very cold.");
+}else if(temperature < 15){
+    console.log("Cold.")
+}else if(temperature < 25) {
+    console.log("Warm.")
+}else{
+    console.log("hot.")
+}
