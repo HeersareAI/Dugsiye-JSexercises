caf0b66a909cb73336f7848148b1398a4e9b829e
HeersareAI <Heersareai@gmail.com>
2026-01-20 15:39:50 +0100

    exercise16

---
+++ exercise16/main.js
@@ -0,0 +1,12 @@
+let score = 62;
+if (score >= 50){
+    console.log("you bassed:");
+}else{
+    console.log("you failed");
+}
+let score2 = 48;
+if (score2 >= 50){
+    console.log("you bassed:");
+}else{
+    console.log("you failed");
+}
