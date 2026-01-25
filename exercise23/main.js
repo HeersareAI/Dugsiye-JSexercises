9331109a4de9f4a3976748e2482bbf606e019360
HeersareAI <Heersareai@gmail.com>
2026-01-25 13:51:24 +0100

    exercise23

--- exercise23/main.js
+++ exercise23/main.js
@@ -1,4 +1,4 @@
-const cars = { make: "Toyota", model: "Corolla", year: 2020 };
+const cars = { make: "Toyota", model: "Corolla", year: 2026 };
 const {make,model,year} = cars;
 console.log(make);
 console.log(model);
