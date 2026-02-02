442ddca2961f58a1a09accab56702aca6ac65830
HeersareAI <Heersareai@gmail.com>
2026-02-02 15:11:20 +0100

    exercise32

---
+++ exercise32/main.js
@@ -0,0 +1,20 @@
+
+
+const title = document.querySelector('.title');
+console.log(title);
+
+
+
+const list = document.querySelector('#list');
+console.log("Single Element:", list);
+
+
+const firstItem = document.querySelector('.item');
+console.log("First Item Only:", firstItem);
+
+
+const allItems = document.querySelectorAll('.item');
+console.log("All Elements (NodeList):", allItems);
+
+
+
